3410a76c340654f4af503417930271085b308a4c
Alexander Dinu <aluuu@husa.su>
2015-07-22 10:52:09 +0500

    use Elements instead of Html as view and handle resizing

--- Main.elm
+++ Main.elm
@@ -1,50 +1,64 @@
 module Main where
-
+import Window
 import Debug
 import Markdown
 import Signal as Signal exposing (Mailbox, Address, mailbox)
-
-import Html exposing (Html, textarea, div, text, fromElement)
+import Graphics.Element exposing (Element, right, flow, size, spacer)
+import Html exposing (Html, textarea, div, text, fromElement, toElement)
 import Html.Attributes exposing (value, rows, cols, style)
 import Html.Events exposing (on, targetValue)
 
-type alias State = {content: String}
+type alias State = {content: String, width: Int, height: Int}
 
-type Action = Reset | Update String
+type Action = Reset | Update String | Resize (Int, Int)
 
 actions : Mailbox Action
 actions = mailbox Reset
 
 emptyState : State
-emptyState = {content=""}
+emptyState = {content="", width=1000, height=500}
 
-view : State -> Html
+editor : State -> Element
+editor state =
+  let
+    onEdit = (\t -> Signal.message actions.address (Update t))
+    styles = [ ("width", "100%")
+                       , ("height", "100%")
+                       , ("padding", "10px")]
+  in
+    textarea [ style styles
+             , value (Debug.watch "updater" state.content)
+             , on "input" targetValue onEdit] []
+    |> toElement (state.width // 3) state.height
+
+preview : State -> Element
+preview state = state.content
+              |> Markdown.toElement
+              |> size (state.width // 3) state.height
+
+view : State -> Element
 view state =
-  div [style [("margin", "50px auto"),
-              ("width", "80%")]]
-        [ textarea [ style [ ("float", "left")
-                           , ("width", "40%")
-                           , ("padding", "10px")]
-                   , rows 50
-                   , cols 40
-                   , value (Debug.watch "updater" state.content)
-                   , on "input" targetValue
-                          (\t -> Signal.message actions.address (Update t))] []
-        , div [style [ ("float", "left")
-                     , ("width", "40%")
-                     , ("padding", "10px")]]
-                [ Markdown.toElement state.content |> fromElement ]]
+  flow right [ editor state
+             , spacer 50 state.height
+             , preview state]
 
 update : Action -> State -> State
 update action state =
   case action of
     Reset -> state
     Update content -> { state | content <- content }
+    Resize (width, height) -> {state | width <- width, height <- height}
+
+resize : Signal Action
+resize =
+  Signal.map Resize Window.dimensions
 
 state : Signal State
 state =
-  Signal.foldp update emptyState actions.signal
+  Signal.mergeMany [actions.signal, resize]
+  |> Signal.foldp update emptyState
 
-main : Signal Html
+
+main : Signal Element
 main =
   Signal.map view state
